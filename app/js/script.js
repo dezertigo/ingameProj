d8a68b5d575d21247d551703142b5354555a6f06
Folomieiev Maksym <Folomieiev.m@gmail.com>
2022-10-05 00:13:57 +0300

    mouse => pointer

--- app/js/script.js
+++ app/js/script.js
@@ -167,4 +167,43 @@ window.addEventListener("load", () => {
    cardContent.forEach((el) => {
       el.style.top = `calc(100% - ${maxPreviewHeight}px)`;
    });
+
+   // ! Footer
+   // Telegram hover
+   document.body.addEventListener("mouseover", changeTelegramColor);
+
+   function changeTelegramColor(e) {
+      if (e.type == "mousedown") {
+         if (e.target.closest(".footer-top__subscribe a")) {
+            e.preventDefault();
+            qa(".footer-top__subscribe .not")[0].classList.remove("hover");
+            qa(".footer-top__subscribe .not")[1].classList.remove("hover");
+            qa(".footer-top__subscribe .not")[0].classList.add("clicked");
+            qa(".footer-top__subscribe .not")[1].classList.add("clicked");
+            document.body.addEventListener("mouseup", removeStylesUp);
+         }
+      } else if (e.type == "mouseover") {
+         if (e.target.closest(".footer-top__subscribe a")) {
+            qa(".footer-top__subscribe .not")[0].classList.add("hover");
+            qa(".footer-top__subscribe .not")[1].classList.add("hover");
+            document.body.addEventListener("mousedown", changeTelegramColor);
+            document.body.addEventListener("mouseout", removeStylesOut);
+         }
+      }
+   }
+   function removeStylesUp(e) {
+      console.log("up");
+      qa(".footer-top__subscribe .not")[0].classList.remove("clicked");
+      qa(".footer-top__subscribe .not")[1].classList.remove("clicked");
+      document.body.removeEventListener("mousedown", changeTelegramColor);
+      document.body.removeEventListener("mouseup", removeStylesUp);
+      document.body.removeEventListener("mouseout", removeStylesOut);
+   }
+   function removeStylesOut(e) {
+      qa(".footer-top__subscribe .not")[0].classList.remove("hover");
+      qa(".footer-top__subscribe .not")[1].classList.remove("hover");
+      document.body.removeEventListener("mousedown", changeTelegramColor);
+      // document.body.removeEventListener("mouseup", removeStylesUp);
+      document.body.removeEventListener("mouseout", removeStylesOut);
+   }
 });
